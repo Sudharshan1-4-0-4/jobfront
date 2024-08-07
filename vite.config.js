f9a353ee3a5c62ba0231acb8c1dfe109ed8f3532
sudharshan <siddumothukuru@gmail.com>
2024-08-07 23:20:57 +0530

    s

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 5173,
     host: '0.0.0.0'
   },
   build: {
